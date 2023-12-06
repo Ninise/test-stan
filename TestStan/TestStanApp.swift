80912e09df7946373f9d1b1f40abd31ebd2f1aab
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-12-06 13:09:07 -0500

    Initial Commit

---
+++ TestStan/TestStanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestStanApp.swift
+//  TestStan
+//
+//  Created by Nikita on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestStanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
